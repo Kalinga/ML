42531181032e3980be8f306fa5850ff12da58cea
Kalinga Ray <mail.kalinga@gmail.com>
2019-07-04 17:19:50 +0200

    Instead of forking,  cloned ssd_keras from pierluigiferrari

---
+++ ssd_keras/ssd7_prediction.py
@@ -0,0 +1,145 @@
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, TerminateOnNaN, CSVLogger
+from keras import backend as K
+from keras.models import load_model
+from math import ceil
+import numpy as np
+from matplotlib import pyplot as plt
+
+from models.keras_ssd7 import build_model
+from keras_loss_function.keras_ssd_loss import SSDLoss
+from keras_layers.keras_layer_AnchorBoxes import AnchorBoxes
+from keras_layers.keras_layer_DecodeDetections import DecodeDetections
+from keras_layers.keras_layer_DecodeDetectionsFast import DecodeDetectionsFast
+
+from ssd_encoder_decoder.ssd_input_encoder import SSDInputEncoder
+from ssd_encoder_decoder.ssd_output_decoder import decode_detections, decode_detections_fast
+
+from data_generator.object_detection_2d_data_generator import DataGenerator
+from data_generator.object_detection_2d_misc_utils import apply_inverse_transforms
+from data_generator.data_augmentation_chain_variable_input_size import DataAugmentationVariableInputSize
+from data_generator.data_augmentation_chain_constant_input_size import DataAugmentationConstantInputSize
+from data_generator.data_augmentation_chain_original_ssd import SSDDataAugmentation
+
+
+# Images
+images_dir = 'datasets/udacity_driving_datasets/'
+
+# Ground truth
+train_labels_filename = 'datasets/udacity_driving_datasets/labels_train.csv'
+val_labels_filename   = 'datasets/udacity_driving_datasets/labels_val.csv'
+
+val_dataset = DataGenerator(load_images_into_memory=False, hdf5_dataset_path=None)
+val_dataset.parse_csv(images_dir=images_dir,
+                      labels_filename=val_labels_filename,
+                      input_format=['image_name', 'xmin', 'xmax', 'ymin', 'ymax', 'class_id'],
+                      include_classes='all')
+
+# 1: Set the generator for the predictions.
+
+predict_generator = val_dataset.generate(batch_size=1,
+                                         shuffle=True,
+                                         transformations=[],
+                                         label_encoder=None,
+                                         returns={'processed_images',
+                                                  'processed_labels',
+                                                  'filenames'},
+                                         keep_images_without_gt=False)
+
+# 2: Generate samples
+
+batch_images, batch_labels, batch_filenames = next(predict_generator)
+
+i = 0 # Which batch item to look at
+
+print("Image:", batch_filenames[i])
+print()
+print("Ground truth boxes:\n")
+print(batch_labels[i])
+
+
+# 3: Make a prediction
+img_height = 300 # Height of the input images
+img_width = 480 # Width of the input images
+img_channels = 3 # Number of color channels of the input images
+intensity_mean = 127.5 # Set this to your preference (maybe `None`). The current settings transform the input pixel values to the interval `[-1,1]`.
+intensity_range = 127.5 # Set this to your preference (maybe `None`). The current settings transform the input pixel values to the interval `[-1,1]`.
+n_classes = 5 # Number of positive classes
+scales = [0.08, 0.16, 0.32, 0.64, 0.96] # An explicit list of anchor box scaling factors. If this is passed, it will override `min_scale` and `max_scale`.
+aspect_ratios = [0.5, 1.0, 2.0] # The list of aspect ratios for the anchor boxes
+two_boxes_for_ar1 = True # Whether or not you want to generate two anchor boxes for aspect ratio 1
+steps = None # In case you'd like to set the step sizes for the anchor box grids manually; not recommended
+offsets = None # In case you'd like to set the offsets for the anchor box grids manually; not recommended
+clip_boxes = False # Whether or not to clip the anchor boxes to lie entirely within the image boundaries
+variances = [1.0, 1.0, 1.0, 1.0] # The list of variances by which the encoded target coordinates are scaled
+normalize_coords = True # Whether or not the model is supposed to use coordinates relative to the image size
+
+model = build_model(image_size=(img_height, img_width, img_channels),
+                    n_classes=n_classes,
+                    mode='training',
+                    l2_regularization=0.0005,
+                    scales=scales,
+                    aspect_ratios_global=aspect_ratios,
+                    aspect_ratios_per_layer=None,
+                    two_boxes_for_ar1=two_boxes_for_ar1,
+                    steps=steps,
+                    offsets=offsets,
+                    clip_boxes=clip_boxes,
+                    variances=variances,
+                    normalize_coords=normalize_coords,
+                    subtract_mean=intensity_mean,
+                    divide_by_stddev=intensity_range)
+model.load_weights('./ssd7_epoch-15_loss-2.1809_val_loss-2.3229.h5')
+print(model.summary())
+
+#load_model('./ssd7_epoch-15_loss-2.1809_val_loss-2.3229.h5')
+y_pred = model.predict(batch_images)
+
+# 4: Decode the raw prediction `y_pred`
+
+y_pred_decoded = decode_detections(y_pred,
+                                   confidence_thresh=0.5,
+                                   iou_threshold=0.45,
+                                   top_k=200,
+                                   normalize_coords=normalize_coords,
+                                   img_height=img_height,
+                                   img_width=img_width)
+
+np.set_printoptions(precision=2, suppress=True, linewidth=90)
+print("Predicted boxes:\n")
+print('   class   conf xmin   ymin   xmax   ymax')
+print(y_pred_decoded[i])
+
+# 5: Draw the predicted boxes onto the image
+
+plt.figure(figsize=(20,12))
+plt.imshow(batch_images[i])
+
+current_axis = plt.gca()
+
+colors = plt.cm.hsv(np.linspace(0, 1, n_classes+1)).tolist() # Set the colors for the bounding boxes
+
+classes = ['background', 'car', 'truck', 'pedestrian', 'bicyclist', 'light'] # Just so we can print class names onto the image instead of IDs
+
+# Draw the ground truth boxes in *green* (omit the label for more clarity)
+for box in batch_labels[i]:
+    xmin = box[1]
+    ymin = box[2]
+    xmax = box[3]
+    ymax = box[4]
+    label = '{}'.format(classes[int(box[0])])
+    current_axis.add_patch(plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, color='green', fill=False, linewidth=1))
+    #current_axis.text(xmin, ymin, label, size='x-large', color='white', bbox={'facecolor':'green', 'alpha':1.0})
+
+# Draw the predicted boxes in *BLUE*
+for box in y_pred_decoded[i]:
+    xmin = box[-4]
+    ymin = box[-3]
+    xmax = box[-2]
+    ymax = box[-1]
+    color = colors[int(box[0])]
+    label = '{}: {:.2f}'.format(classes[int(box[0])], box[1])
+    current_axis.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, color=color, fill=False, linewidth=1))
+    #current_axis.add_patch(plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, color='blue', fill=False, linewidth=2))
+    current_axis.text(xmin, ymin, label, size='x-large', color='white', bbox={'facecolor':color, 'alpha':1.0})
+plt.show()
